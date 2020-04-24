28a99d500a033b9fb227ac2922d47858e8e74ab1
iquery-001 <kota@vaga.tk>
2020-04-24 10:54:35 -0500

    Done.

--- .history/app/controllers/birds_controller_20200424104755.rb
+++ .history/app/controllers/birds_controller_20200424104755.rb
@@ -1,11 +1,11 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds, only: [:id, :name, :species]
+    render json: birds
   end
  
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird.slice(:id, :name, :species)
+    render json: bird
   end
 end
